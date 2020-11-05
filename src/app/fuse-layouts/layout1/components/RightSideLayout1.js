749f6fb8bba1a4030e68a09706b57bbf9b730f9e
ddtran30 <ddtran30@gmail.com>
2020-11-05 17:36:44 +0700

    skeleton commit

--- src/app/fuse-layouts/layout1/components/RightSideLayout1.js
+++ src/app/fuse-layouts/layout1/components/RightSideLayout1.js
@@ -1,12 +1,9 @@
-import ChatPanel from 'app/fuse-layouts/shared-components/chatPanel/ChatPanel';
-import QuickPanel from 'app/fuse-layouts/shared-components/quickPanel/QuickPanel';
 import React from 'react';
+import QuickPanel from 'app/fuse-layouts/shared-components/quickPanel/QuickPanel';
 
 function RightSideLayout1(props) {
 	return (
 		<>
-			<ChatPanel />
-
 			<QuickPanel />
 		</>
 	);
